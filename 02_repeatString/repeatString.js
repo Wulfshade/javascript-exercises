ffd5362fe6e2177a3dd1ef27ce32677abf2c216a
Wulfshade <Wulfshade@users.noreply.github.com>
2022-03-13 15:57:00 +0000

    02_repeatString done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
+const repeatString = function(text, repeat) {
+    if (repeat < 0) return 'ERROR';
 
+    result = '';
+    for (let i = 0;i<repeat; i++) {
+        result += text;
+    }
+    return result;
 };
 
 // Do not edit below this line
