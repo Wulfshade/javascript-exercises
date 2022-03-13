d57bfae2652a863db1134352c4ad019d22eb5da3
Wulfshade <Wulfshade@users.noreply.github.com>
2022-03-13 20:58:59 +0000

    05_sumAll done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,12 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    if (typeof(num1) != "number" || typeof(num2) != "number" || num1 < 0 || num2 < 0) {
+        return 'ERROR';
+    } else {
+        sum = 0;
+        let range = [num1,num2].sort()
+        for(i=range[0]; i<= range[1]; i++) sum += i;
+        return sum;
+    }
 };
 
 // Do not edit below this line
