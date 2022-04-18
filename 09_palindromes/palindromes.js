e65a95ecf706b087443f868e0d9f09b943d2fe06
Ferdinando Brito <fantasyworldpt@gmail.com>
2022-04-18 19:28:56 +0100

    09_palindromes done

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,12 @@
-const palindromes = function () {
+const palindromes = function (text) {
+  const regex = /[^A-Za-z0-9]/g;
+  const cleanedText = text.replace(regex, "").toLowerCase();
+  const stringSize = cleanedText.length;
+
+  const firstHalf = cleanedText.slice(0, Math.floor(stringSize/2));
+  const secondHalf = cleanedText.slice(Math.ceil(stringSize/2), stringSize).split("").reverse().join("");
+
+  return firstHalf === secondHalf;
 
 };
 
