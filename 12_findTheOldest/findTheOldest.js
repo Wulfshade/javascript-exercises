d033885e9c026e9ee937e6ca4b553efe39fa050b
Ferdinando Brito <fantasyworldpt@gmail.com>
2022-04-19 00:09:12 +0100

    12_findTheOldest done

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,11 @@
-const findTheOldest = function() {
-
-};
+const findTheOldest = people => people.reduce(
+  (prevPerson, currentPerson) => {
+      let currentYear = new Date().getFullYear();
+    let prevPersonAge = (prevPerson.yearOfDeath ? prevPerson.yearOfDeath : currentYear) - prevPerson.yearOfBirth;
+    let currentPersonAge = (currentPerson.yearOfDeath ? currentPerson.yearOfDeath : currentYear) - currentPerson.yearOfBirth;
+    return (prevPersonAge > currentPersonAge) ? prevPerson : currentPerson;
+  }
+);
 
 // Do not edit below this line
 module.exports = findTheOldest;
